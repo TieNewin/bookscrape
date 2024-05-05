7027aa9cccc0e28ba4d53a6cbc52b51c4ce48148
TieNewin <125307533+TieNewin@users.noreply.github.com>
2024-05-05 01:07:33 -0500

    Update bookscrape.py

--- bookscrape.py
+++ bookscrape.py
@@ -1,7 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-phaseOneURL = "https://books.toscrape.com/"
+phaseOneURL = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 phaseOnePage = requests.get(phaseOneURL)
 phaseOneSoup = BeautifulSoup(phaseOnePage.content, 'html.parser')
 
+tableData = phaseOneSoup.find_all("td")
+upc = tableData[0]
+priceTax = tableData[2]
+priceNoTax = tableData[3]
+quantity = tableData[5]
+
+title = phaseOneSoup.find("h1")
+
+print(tableData)
+
+print(phaseOneURL)
+print(upc.text)
+print(title.text)
+print(priceTax.text)
+print(priceNoTax.text)
+print(quantity.text)
